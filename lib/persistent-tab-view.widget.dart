45eaf1e06b391154a48ebdff928fa65801d82870
Sarp Güney Başaraner <sarpbasaraner@gmail.com>
2021-02-05 19:54:23 +0300

    Fix invalid assertion
    This assertion should not be using items.length since items will always be null here. This currently fails all cases where routeAndNavigatorSettings is not null.

--- lib/persistent-tab-view.widget.dart
+++ lib/persistent-tab-view.widget.dart
@@ -193,7 +193,7 @@ class PersistentTabView extends PersistentTabViewBase {
     assert(
         routeAndNavigatorSettings.navigatorKeys == null ||
             routeAndNavigatorSettings.navigatorKeys != null &&
-                routeAndNavigatorSettings.navigatorKeys.length != items.length,
+                routeAndNavigatorSettings.navigatorKeys.length != itemCount,
         "Number of 'Navigator Keys' must be equal to the number of bottom navigation tabs.");
   }
 }
