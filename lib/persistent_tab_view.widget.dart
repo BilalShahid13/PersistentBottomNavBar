83566ce7a8ed70497d67bef736b0ab7e486bebe0
Deepak Rai <103811779+StrawHATDEEPAK@users.noreply.github.com>
2023-06-30 02:27:32 +0530

    Update persistent_tab_view.widget.dart
    Fixed OnWillPop Assert Error When Using Custom Persistent Tab View
    By Changing OnWillPop != null to OnWillPop == null To correct the condition which invoked the asset throw

--- lib/persistent_tab_view.widget.dart
+++ lib/persistent_tab_view.widget.dart
@@ -104,7 +104,7 @@ class PersistentTabView extends PersistentTabViewBase {
     this.screenTransitionAnimation = const ScreenTransitionAnimation(),
   })  : assert(itemCount == screens.length,
             "screens and items length should be same. If you are using the onPressed callback function of 'PersistentBottomNavBarItem', enter a dummy screen like Container() in its place in the screens"),
-        assert(handleAndroidBackButtonPress && onWillPop != null,
+        assert(handleAndroidBackButtonPress && onWillPop == null,
             "If you declare the onWillPop function, you will have to handle the back function functionality yourself as your onWillPop function will override the defualt function."),
         super(
           key: key,
