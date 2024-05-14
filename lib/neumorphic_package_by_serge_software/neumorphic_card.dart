8e631ae584ce974bf694e9fba40c1524af2922b2
Dr. Pshtiwan <kardoey@gmail.com>
2024-05-14 03:19:09 +0300

    Update neumorphic_card.dart

--- lib/neumorphic_package_by_serge_software/neumorphic_card.dart
+++ lib/neumorphic_package_by_serge_software/neumorphic_card.dart
@@ -54,7 +54,7 @@ class NeumorphicContainer extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
-    final color = decoration.color ?? Theme.of(context).backgroundColor;
+    final color = decoration.color ?? Theme.of(context).scaffoldBackgroundColor;
     final emboss = curveType == CurveType.emboss;
 
     Color colorValue = color;
